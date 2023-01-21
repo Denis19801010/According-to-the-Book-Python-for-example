235b002974b225404ba2de808e0318a21224b1d5
Денис-ПК <denisbataev351@gmail.com>
2023-01-21 15:12:58 +0300

    done on half

---
+++ Code Basics.py
@@ -0,0 +1,62 @@
+# 1
+# name = input('Введите имя >>> ')
+# print(' Hello', name)
+#
+# #2
+# name = input('Введите имя >>>')
+# surname = input(' Введите фамилию >>> ')
+# print('Hello ', name, surname)
+
+# 3
+# print('What are you call a bear with no teeth? \n A gummy bear!')
+
+# 4
+# num1 = int(input('Введи первое число >>>'))
+# num2 = int(input('Введи первое число >>>'))
+# summa = int(num1 + num2)
+# print('The total is:', summa)
+
+# 5
+# num1 = int(input('Введи первое число >>> '))
+# num2 = int(input('Введи второе число >>> '))
+# num3 = int(input('Введи третье число >>> '))
+# itog = (num1 + num2) * num3
+# print('The answer is:', itog)
+
+# 6
+# How_much_was = int(input('Сколько было пиццы скажи: '))
+# How_much_eat = int(input('Сколько схавал?: '))
+# How_much_stay = How_much_was - How_much_eat
+# print('У тебя осталось - ', How_much_stay, ' кусков')
+
+# # 7
+# Name = input('Введи свое имя: ')
+# Age = int(input('Введи свой возраст: '))
+# print(Name, 'next birthday you will be ', Age + 1)
+
+# #8
+# total_score = int(input('введи всю сумму на которую погуляли >>> '))
+# total_people = int(input('Сколько всего было народу >>> '))
+# print('Каждый торчит: ', total_score/total_people, 'руб')
+
+
+
+#9
+total_time = int(input('Введи время в днях - '))
+
+print(total_time)
+#(1 час = 1/24 дня (суток) = 0,041(6) дней (суток) 1 день (сутки) = 24 часа
+0,041 дня = 1 час
+print('в веденных данных ', 'часов', 'минут', 'секунд')
+
+# #10
+# phount = 2.204
+# input_value = int(input('Введи вес в килограммах >>> '))
+# kg_ph = input_value * phount
+# print(round(kg_ph, 2), 'фунтов')
+
+# #11
+# num1 = int(input('Введи число больше 100 >>> '))
+# num2 = int(input('введи число меньше 10 >>> '))
+# raz = num1//num2
+# print('второе число ', raz, ' раз помещается в меньшем')
