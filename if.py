61bde8cde6b1ee192a395af788c5b2c7fb053c79
Денис-ПК <denisbataev351@gmail.com>
2023-01-21 18:13:54 +0300

    done

---
+++ if.py
@@ -0,0 +1,79 @@
+#12
+# num1 = int(input('Enter number one >>> '))
+# num2 = int(input('Enter number two >>> '))
+# if num1 > num2:
+#     print(num2, num1)
+# else:
+#     print(num1, num2)
+#
+#13
+
+# num = int(input('Enter number less than 20 >>> '))
+# if num >= 20:
+#     print('Too high')
+# else:
+#     print('Thank you')
+
+#14
+# num = int(input('Enter number from 10 to 20(inclusive) >>> '))
+# if 10 < num <= 20:
+#     print('Thank you')
+# else:
+#     print('Incorrect answer')
+
+#15
+# color = (input(' Enter color >>> ')).lower()
+# # if color == 'red':
+#     print('I like red too')
+# else:
+#     print('I don`t like', color, ', i prefer red')
+
+#16
+# rain = input('Is it raining outside?: ').lower()
+# if rain == 'yes':
+#     windy = input('Is it windy outside?: ').lower()
+#     if windy == 'yes':
+#         print('It is so windy for an umbrella')
+#     else:
+#         print('Take an umbrella')
+# else:
+#     print('Enjoy your day')
+
+#17
+# age = int(input('What is your age?: '))
+# if age >= 18:
+#     print('You can vote')
+# elif age == 17:
+#     print('You can learn to drive')
+# elif age == 16:
+#     print('You can buy a lottery ticket')
+# else:
+#     print('You can go Trick-or-Treating')
+
+#18
+# num = int(input('Enter number >>> '))
+# if 10 < num < 20:
+#     print('Correct')
+# elif num < 10:
+#     print('too low')
+# else:
+#     print('Too high')
+# #19
+# num = int(input(' Enter 1, 2 or 3: '))
+# if num == 1:
+#     print('Thank you')
+# elif num == 2:
+#     print('Well done')
+# elif num == 3:
+#     print('Correct')
+# else:
+#     print('Error message')
+
+
+
+
+
+
+
+
+
