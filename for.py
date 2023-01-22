0a3fad11cfb12c50111186ea1cbaa5b4e05018ea
Денис-ПК <denisbataev351@gmail.com>
2023-01-22 05:56:12 +0300

    don`t complete

--- for.py
+++ for.py
@@ -11,8 +11,24 @@
 #     print(name)
 
 # 37 Предложите пользователю ввести имя. Выведите каждую букву имени в отдельной строке.
+#
+# name = input('Enter your name >>> ')
+# for name in name:
+#     print(name)
+
+# ДРУГОЕ РЕШЕНИЕ
+
+# name = input('Enter your name >>> ')
+# for i in name:
+#     print(i)
+
+# 38 Измените программу из упражнения 37 так, чтобы она также запрашивала число. Выведите
+# имя (по одной букве в каждой строке) и по вторите вывод равное введенному числу коли-
+# чество раз.
 
 name = input('Enter your name >>> ')
-for :
-    n
-    print(name)
+num = int(input('Enter number >>> '))
+for k in range(0, num):
+    for i in name:
+        print(i)
+
